539ff8569f3adae9ef38a2dee40ae5fd5c642aab
akihiro0710 <acedia2074@gmail.com>
2018-06-26 03:29:30 +0900

    add output config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,13 @@ module.exports = {
 
   // メインとなるJavaScriptファイル（エントリーポイント）
   entry: './src/main.ts',
-
+  // ファイルの出力設定
+  output: {
+    //  出力ファイルのディレクトリ名
+    path: `${__dirname}/dist`,
+    // 出力ファイル名
+    filename: 'main.js'
+  },
   module: {
     rules: [
       {
@@ -20,6 +26,6 @@ module.exports = {
   resolve: {
     extensions: [
       '.ts'
-    ]
+    ],
   }
 };
